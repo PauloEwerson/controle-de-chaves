6f848be932a8e865db0de3ba86ffa7e68371c377
Ewerson Martins <ewersonmrt@gmail.com>
2022-08-10 21:06:15 -0300

    Cria conecção com o BD

---
+++ models/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connection = mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'senha123',
+  database: 'controle_de_chaves',
+  port: process.env.DB_PORT || 3306,
+});
+
+module.exports = connection;
